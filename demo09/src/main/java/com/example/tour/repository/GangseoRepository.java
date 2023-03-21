ad4e3b8fecf0a20be343d08f136f9ad405d680cf
CoDiWolf <114123369+CoDiWolf@users.noreply.github.com>
2023-03-21 14:44:55 +0900

    Delete demo09 directory

--- demo09/src/main/java/com/example/tour/repository/GangseoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tour.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.tour.model.Gangseo;
-
-public interface GangseoRepository extends JpaRepository<Gangseo, Long> {
-
-}
--- demo09/src/main/java/com/example/tour/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tour.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.tour.model.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long>{
-	 Member findByUsername(String username);
-}
